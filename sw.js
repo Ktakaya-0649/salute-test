0bf1ab3b12fd02c36251b80fd782e695311c02ec
Ktakaya-0649 <K.takaya04649@outlook.com>
2026-09-18 16:13:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* SALUTE service worker
    ★ アプリを更新したら、必ず下の CACHE の数字を上げること。
       上げ忘れると、ホーム画面から開いたときに古い画面がキャッシュから返る。 */
-const CACHE = "salute-v22";
+const CACHE = "salute-v23";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
